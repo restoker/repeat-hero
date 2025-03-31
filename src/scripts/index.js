7ee1b58eae85aa2f59e93a8a6e11b55c0c34c600
restoker <milthon.doom@gmail.com>
2025-03-30 20:20:58 -0500

    image and letter fixed and added letters animation

---
+++ src/scripts/index.js
@@ -0,0 +1,31 @@
+import gsap from "gsap";
+import SplitType from "split-type";
+
+
+window.addEventListener('load', () => {
+    const title1 = document.querySelector('.title1');
+    const title2 = document.querySelector('.title2');
+    const splitTitle1 = new SplitType(title1);
+    const splitTitle2 = new SplitType(title2);
+
+
+    const tl = gsap.timeline();
+
+    tl
+        .from(splitTitle1.chars, {
+            yPercent: -100,
+            stagger: {
+                each: 0.2,
+                from: 'start',
+            },
+            // ease: 'back.out'
+        })
+        .from(splitTitle2.chars, {
+            yPercent: 100,
+            stagger: {
+                each: 0.2,
+                from: 'end',
+            },
+            // ease: 'back.out'
+        }, '<')
+});
